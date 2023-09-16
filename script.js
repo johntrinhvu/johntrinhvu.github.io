5d6e0abe4e0a0f1a7e355cbb0a818ca5bd4e5c66
John Vu <johntrinhvu@yahoo.com>
2023-09-16 18:48:50 -0400

    restart from scratch

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
 // NAVBAR
+const navLinkEls = document.querySelectorAll('.nav-link');
+const sectionEls = document.querySelectorAll('.section');
 
-let navbar = document.getElementById("main-nav");
-let viewportHeight = window.innerHeight;
-let navHeight = document.getElementById("main-nav").offsetHeight;
+// CHANGE ACTIVE COLOR WHEN SCROLL
+let currentSection = 'home';
+window.addEventListener('scroll', () => {
+    sectionEls.forEach(sectionEl => {
+        if (window.scrollY >= (sectionEl.offsetTop - sectionEl.clientHeight / 2)) {
+            currentSection = sectionEl.id;
+        }
+    });
 
-let navbarLinks = document.querySelectorAll("nav a");
-
-window.addEventListener("scroll", e => {
-  scrollpos = window.scrollY;
-  navbarLinks.forEach(link => {
-    let div = document.querySelector(link.hash);
-    if (div.offsetTop <= scrollpos + 150 &&
-        div.offsetTop + div.offsetHeight > scrollpos + 150) {
-      link.classList.add("active");
-    } else {
-      link.classList.remove("active");
-    }
-  });
+    navLinkEls.forEach(navLinkEl => {
+        if (navLinkEl.textContent.toLowerCase() === currentSection) {
+            document.querySelector('.active').classList.remove('active');
+            navLinkEl.classList.add('active');
+        }
+    });
 });
+
+
+// Function to scroll to a specific section
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
+// Add a click event listener to each navigation link
+navLinkEls.forEach(navLinkEl => {
+    navLinkEl.addEventListener('click', (event) => {
+        // Prevent the default link behavior (e.g., following a URL)
+        event.preventDefault();
+
+        // Get the target section's ID based on the link's text content
+        const targetSectionId = event.target.textContent.toLowerCase();
+
+        // Scroll to the target section
+        scrollToSection(targetSectionId);
+    });
+});
+
