8acc44eb3fba15488dffcde4ddc9a63bf1addc6e
johntrinhvu <johntrinhvu@yahoo.com>
2024-08-13 05:30:02 -0700

    maybe fix issue

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -52,7 +52,7 @@ export default function Project(props) {
                         <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
                             {tags.map((tag, index) => (
                                 <li 
-                                    className="bg-violet-950 px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white/70 rounded-full" 
+                                    className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white/70 rounded-full" 
                                     key={index}
                                 >
                                     {tag}
