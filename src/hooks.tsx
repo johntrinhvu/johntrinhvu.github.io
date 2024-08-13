147d3a31ff0b8525493f450be7dd3aa76db96900
johntrinhvu <johntrinhvu@yahoo.com>
2024-08-13 05:33:23 -0700

    finish for now

--- src/hooks.tsx
+++ src/hooks.tsx
@@ -1,11 +1,7 @@
 import { useInView } from "react-intersection-observer";
 import { useActiveSectionContext } from "./context/ActiveSectionContext/ActiveSectionContext.tsx";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import type { SectionName } from "./types.tsx";
-
-type useSectionInViewProps = {
-    sectionName: SectionName;
-};
 
 export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
     const { ref, inView } = useInView({
